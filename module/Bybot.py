f2d38935610d8df4201bd1c94c9e6f68f58ed489
N/Name <87293285+0xCurtis@users.noreply.github.com>
2022-08-01 02:14:32 +0200

    Add files via upload

--- module/Bybot.py
+++ module/Bybot.py
@@ -1,8 +1,11 @@
+from email.policy import HTTP
 import json
 import os
-import pyfiglet
-import pybit.inverse_futures
+import pybit.usdt_perpetual
 import pathlib
+from datetime import datetime
+from threading import Thread, Event
+import time
 
 class ByBot:
     def __init__(self) -> None:
@@ -12,20 +15,97 @@ class ByBot:
                 self._api_key = cfg['creds']['api_key']
                 self._api_secret = cfg['creds']['api_secret']
                 self.endpoint = cfg['parameters']['endpoint']
-                self.session = pybit.inverse_futures.HTTP(endpoint=self.endpoint, api_key=self._api_key, api_secret=self._api_secret)
+                self.session = pybit.usdt_perpetual.HTTP(endpoint=self.endpoint, api_key=self._api_key, api_secret=self._api_secret)
+                
                 self.id = cfg['logging']['id']
-                self.logpath = "./logs/{}".format(self.id)
                 self.desc = cfg['logging']['desc']
                 self.port = cfg['network']['port']
+                
+                self.leverage = cfg['parameters']['leverage']
+                self.max_size = cfg['parameters']['size']['max']
+                self.flat = cfg['parameters']['size']['flat']
+                self.size = cfg['parameters']['size']['size']
+                self.logpath = "./logs/{}".format(self.id)
+                self.logfile = self.logpath + "/logging.txt"
+                self.make_log_header()
                 pathlib.Path(self.logpath).mkdir(parents=True, exist_ok=True)
+                self.in_trade = False
+
+                self.thread = Thread(target=self.order_manager, daemon=True)
         except Exception as e:
             print("Error while initialising the bot - {}".format(e))
             exit(84)
-    def run(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
-        os.system('title ByBot V0.1')
-        print(pyfiglet.figlet_format("ByBot V 0.1", font="slant"))
-        app.run(debug=True, host="localhost", port=self.port)
+    def order_manager(self):
+        for a in range(500):
+            time.sleep(0.1)
+            print(a)
+    def make_log_header(self):
+        balance = self.get_balance()
+        with open(self.logfile, 'a') as log:
+            log.writelines("Purpose : {}\nID : {}\nEndpoint : {}\nLaunched on : {}\nBalance : {} USDT\n{}\n\n".format(self.desc, self.id, self.endpoint, datetime.now(), balance,'-'*30)) 
+    
+    def log(self, str):
+        date_time = datetime.now()
+        date_str = date_time.strftime("- %d/%m/%Y, %H:%M:%S:%f")
+        with open(self.logfile, 'a') as log:
+            log.writelines(date_str + " : " + str + '\n')
+
+    def get_last_price(self, pair):
+        price = self.session.public_trading_records(
+            symbol=pair,
+            limit=1
+        )['result'][0]['price']
+        return price
+    
+    def set_margin(self, buy=1, sell=1, pair=""):
+        try:
+            self.session.set_leverage(
+                symbol=pair,
+                buy_leverage=buy,
+                sell_leverage=sell)
+        except Exception as e:
+            pass
+
+    def get_balance(self):
+        return self.session.get_wallet_balance(coin="USDT")['result']['USDT']['available_balance']
+
+    def open_from_data(self, data):
+        self.log("Request of {} on {} pair".format(data['side'], data['pair']))
+        if self.flat == False:
+            balance = self.get_balance()
+            qty_in_usd = balance*(qty_in_usd/100)
+        qty_in_usd = min(self.size, self.max_size)
+        self.open_perp_order(pair=data['pair'], side=data['side'], qty_in_usd=qty_in_usd, lever=self.leverage, flat=True)
+
+    def open_perp_order(self, pair="", side="", qty_in_usd=0, lever=1, flat=True, sl=None, tp=None):
+        l_price = self.get_last_price(pair)
+        quantity = round((qty_in_usd / l_price) * lever, 5)
+        #stop = l_price * 0.99 if side == "Buy" else l_price * 1.01
+        take = None
+        stop = None
+        self.set_margin(buy=lever, sell=lever, pair=pair)
+        
+        res = self.session.place_active_order(
+            symbol=pair,
+            side=side,
+            order_type="Market",
+            qty=quantity,
+            time_in_force="GoodTillCancel",
+            reduce_only=False,
+            close_on_trigger=False,
+            stop_loss=stop,
+            take_profit=take
+            )
+        self.in_trade = True
+        return res
 
     def __repr__(self) -> str:
         return("ByBot V0.1\n\tEndpoint : {}\n\tID : {}\n\tLogpath : {}\n\tDescription : {}".format(self.endpoint, self.id, self.logpath, self.desc))
+
+if __name__ == "__main__":
+    bot = ByBot()
+    bot.thread.start()
+    print("Yes")
+    time.sleep(10)
+    print("No")
+    bot.thread.join()
