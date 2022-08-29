0e9aa2b051f4d3a89a7ff954c2a725b07ceada39
Thibb1 <thibault.brumaire@gmail.com>
2022-08-29 09:15:41 +0200

    * balance checking

--- module/BB.py
+++ module/BB.py
@@ -51,6 +51,7 @@ class ByBot:
 
             self.updating = False
             self.trading = False
+            self.balance = 0
             self.name = cfg['name']
             self.network = cfg['network']
             self.leverage = order['leverage']
@@ -102,6 +103,8 @@ class ByBot:
 
     def update(self):
         try:
+            self.balance = self.session.get_wallet_balance()
+            self.max = min(self.balance, self.max)
             position = self.session.my_position(symbol=self.symbol)['result']
             leverage = self.leverage
             if position[0]['leverage'] != leverage or position[1]['leverage'] != leverage:
